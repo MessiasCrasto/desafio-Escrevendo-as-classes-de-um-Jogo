ebf92d599377d1a03cab16059c1bbd8fcfcd110a
Messias Crasto <messiascrasto40@gmail.com>
2024-02-22 20:23:41 -0300

    final

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-/*
-## Objetivo:
-Crie uma classe generica que represente um herói de uma aventura e que
-possua as seguintes propriedades:
-- nome - idade - tipo (ex: guerreiro, mago, monge, ninja )
-além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
-- exibir a mensagem: "o {tipo} atacou usando {ataque}")
-- aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
-- e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
-
-se mago -> no ataque exibir (usou magia)
-se guerreiro -> no ataque exibir (usou espada)
-se monge -> no ataque exibir (usou artes marciais)
-se ninja -> no ataque exibir (usou shuriken)
-## Saída
-Ao final deve se exibir uma mensagem:
-- "o {tipo} atacou usando {ataque}"
-  ex: mago atacou usando magia
-  guerreiro atacou usando espada
-*/
-
-class hero {
+class Hero {
     constructor(nome,idade,tipo){
     this.nome = nome
     this.idade = idade
     this.tipo = tipo
     }
-    atacar(){
-    
+    especialidade() {
+        let ataque=""
+    if (this.tipo == "mago"){
+    ataque = "magia"
+    }else if (this.tipo == "guerreiro"){
+    ataque = "espada"
+    }else if (this.tipo == "monge"){
+    ataque = "artes marciais"
+    }else if (this.tipo == "ninja"){
+    ataque = "shuriken"
+    }
+    return ataque
+    }
+    atacar() {
+    console.log(`o ${this.tipo} atacou usando ${this.especialidade()}`)
+    return
     }
 }
 
-let novoHero = new hero("Jiraya",32,"ninja")
-console.log(novoHero.nome)
+let novoHero = new Hero("Jiraya",32,"ninja")
+novoHero.atacar()
